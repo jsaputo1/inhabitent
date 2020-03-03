1867b2c396d49aa3c080c6e32ee0fea8bc85be9e
John Saputo <John@John-MBP.local>
2020-03-03 16:35:10 -0500

    style: styled product type page

--- themes/inhabitant/taxonomy-product-type.php
+++ themes/inhabitant/taxonomy-product-type.php
@@ -1,22 +1,45 @@
 
 <?php get_header(); ?>
 
-<p>Test</p>
+<div class="content-container">
+<!-- header -->
+<div class="product-header">
+    <h1>Shop Stuff</h1>
+    <p><?php wp_nav_menu(array(
+            'theme_location' => 'product'
+            )) ;?>  
+</div>
 
 
+    <section class="product-page">
 
-<?php if( have_posts() ) :
-//The WordPress Loop: loads post content 
-    while( have_posts() ) :
-        the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_post_thumbnail(); ?>
-    <h3><?php the_permalink();?></h3>
-    <?php the_content(); ?>
+        <!-- Loop -->
+        <?php if( have_posts() ) :
+    //The WordPress Loop: loads post content 
+        while( have_posts() ) :
+            the_post(); ?>
+
+        <!-- Products -->
+        <div class="item-container">
+            <div class="shop-pic">
+                <a href="<?php the_permalink();?>"><?php the_post_thumbnail(); ?></a></h2>
+            </div>
+            <div class="product">
+                <span class="title"><?php the_title(); ?></span>
+                <span class="price"> <?php echo '$' . get_field('price');?></span>
+            </div>
+        </div>
+
+
+        
+
     <!-- Loop ends -->
     <?php endwhile;?>
-    <?php the_posts_navigation();?>
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif;?>
+        <?php the_posts_navigation();?>
+    <?php else : ?>
+            <p>No posts found</p>
+    <?php endif;?>
+    </section>
+</div>
 <?php get_footer();?>
+
