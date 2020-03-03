ed565e78979934d7a6722eb8893b0e7d21dd34f4
John Saputo <John@John-MBP.local>
2020-03-03 14:57:08 -0500

    style: added border to header and journal page

--- themes/inhabitant/index.php
+++ themes/inhabitant/index.php
@@ -1,41 +1,41 @@
 <?php get_header(); ?>
 
 <section class="journal">
+    <div class="left-side">
+        <!-- Loop -->
+        <?php if( have_posts() )  
+            while( have_posts() ) :
+                the_post(); ?>
 
-    <!-- Loop -->
-    <?php if( have_posts() )  
-        while( have_posts() ) :
-            the_post(); ?>
-
-    <!-- Blog Banner -->
-    <div class="blog-banner-container" 
-        style="background-image: 
-        url(<?php echo get_the_post_thumbnail_url();?>)">
-        <div class="blog-title">
-            <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-        </div>
-        <div class="blog-bottom-text">
-            <p>
-            <?php echo get_the_date() . " /"  ?>  BY <?php the_author(); ?>
-            </p>
-        </div>
-    </div>
-    <!-- Blog Content -->
-    <div class="blog-content">
-            <p><?php the_content(); ?></p>
-            <div class="journal-button">
-            <a href="<?php the_permalink();?>">Read more →</a>
+        <!-- Blog Banner -->
+        <div class="blog-banner-container" 
+            style="background-image: 
+            url(<?php echo get_the_post_thumbnail_url();?>)">
+            <div class="blog-title">
+                <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+            </div>
+            <div class="blog-bottom-text">
+                <p>
+                <?php echo get_the_date() . " /"  ?>  BY <?php the_author(); ?>
+                </p>
             </div>
         </div>
+        <!-- Blog Content -->
+        <div class="blog-content">
+                <p><?php the_content(); ?></p>
+                <div class="journal-button">
+                <a href="<?php the_permalink();?>">Read more →</a>
+                </div>
+            </div>
+        
+    <!-- Loop ends -->
+    <?php endwhile;?>
+        <?php the_posts_navigation();?>
     
-<!-- Loop ends -->
-<?php endwhile;?>
-    <?php the_posts_navigation();?>
-   
-
-    <div class="sidebar-journal">
+    </div>
+ 
     <?php get_sidebar();?>
-</div>
+
 
 
 
